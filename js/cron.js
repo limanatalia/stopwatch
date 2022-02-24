48027312836f8b639360a3465de3084c738a3b15
Natalia Lima <natalima.al@gmail.com>
2022-02-24 20:04:41 -0300

    add arquivos

---
+++ js/cron.js
@@ -0,0 +1,48 @@
+"use strict"
+
+var hh = 0;
+var mm = 0;
+var ss = 0;
+
+var tempo = 1000;
+var cron;
+
+function start() {
+    cron = setInterval(() => { timer(); }, tempo);
+}
+
+function pause() {
+    clearInterval(cron);
+}
+
+function stop() {
+    clearInterval(cron);
+    hh = 0;
+    mm = 0;
+    ss = 0;
+
+    document.getElementById('counter').innerText = '00:00:00';
+}
+
+function timer() {
+    ss++;
+
+    if (ss == 59) { //Verifica se deu 59 segundos
+        ss = 0; //Volta os segundos para 0
+        mm++; //Adiciona +1 na variável mm
+
+        if (mm == 59) { //Verifica se deu 59 minutos
+            mm = 0; //Volta os minutos para 0
+            hh++; //Adiciona +1 na variável hora
+        }
+    }
+
+    //Cria uma variável com o valor tratado HH:MM:SS
+    var format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
+
+    //Insere o valor tratado no elemento counter
+    document.getElementById('counter').innerText = format;
+
+    //retorna o valor tratado
+    return format;
+}
